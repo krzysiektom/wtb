cadfc272d5a2a842e4e8315d32f644c87ba314d7
krzysztof <krzysiektom@op.pl>
2020-01-05 12:45:18 +0100

    Initial commit

---
+++ src/main/java/com/wtb/WtbApplication.java
@@ -0,0 +1,13 @@
+package com.wtb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WtbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wtb/WtbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wtb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
