a09cb17db3873539e3bc622125841d8dca5b97f9
krzysztof <krzysiektom@op.pl>
2020-01-14 23:44:27 +0100

    Replace to ResponseEntity in add method

--- src/main/java/com/wtb/controller/ColorController.java
+++ src/main/java/com/wtb/controller/ColorController.java
@@ -2,10 +2,14 @@ package com.wtb.controller;
 
 import com.wtb.domain.color.Color;
 import com.wtb.domain.color.ColorRepository;
+import com.wtb.domain.color.ColorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Controller
 @RequestMapping("color")
@@ -13,22 +17,18 @@ import org.springframework.web.bind.annotation.*;
 public class ColorController {
 
     private final ColorRepository colorRepository;
+    private final ColorService colorService;
 
     @ResponseBody
     @PostMapping("/add")
-    public Color add(@RequestBody Color color) {
-        return colorRepository.save(color);
+    public ResponseEntity<Color> add(@RequestBody Color color) throws URISyntaxException {
+        Color colorSaved = colorRepository.save(color);
+        return ResponseEntity.created(new URI("color/" + color.getId())).body(colorSaved);
     }
 
     @ResponseBody
-    @GetMapping()
-    public ResponseEntity<Color> findById(@RequestParam("id") String id) {
-        Color color;
-        try {
-            color = colorRepository.findBy(Long.parseLong(id));
-        } catch (NumberFormatException nfe) {
-            return ResponseEntity.badRequest().build();
-        }
-        return ResponseEntity.ok(color);
+    @GetMapping("/{id}")
+    public ResponseEntity<Color> findById(@PathVariable("id") String id) {
+        return ResponseEntity.ok(colorService.findById(id));
     }
 }
