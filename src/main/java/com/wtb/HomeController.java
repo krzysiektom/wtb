3ec87058c47c7341f65012128b1061c8c583d3c1
krzysztof <krzysiektom@op.pl>
2020-02-08 14:14:26 +0100

    add home controller

---
+++ src/main/java/com/wtb/HomeController.java
@@ -0,0 +1,24 @@
+package com.wtb;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes({"title","name"})
+public class HomeController {
+    @Value("${spring.application.title}")
+    String title;
+
+    @Value("${spring.application.name}")
+    String name;
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("title", title);
+        model.addAttribute("name", name);
+        return "home";
+    }
+}
