a0b6a5030c7a97ec07e622d3dd64a9ceac2ae8bf
krzysztof <krzysiektom@op.pl>
2020-03-03 00:03:30 +0100

    add activating navbar depending on returning page

---
+++ src/main/resources/static/js/home.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    //$('.navbar-nav li.active').removeClass('active');
+    $('#home').addClass('active');
+
+});
--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -1,9 +1,3 @@
 $(document).ready(function () {
-    $('.navbar-nav li a').click(function(e) {
-
-        $('.navbar-nav li.active').removeClass('active');
-
-        var $parent = $(this).parent();
-        $parent.addClass('active');
-    });
+    $('.navbar-nav li.active').removeClass('active');
 });
